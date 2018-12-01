749025277436ac0c3262c8793338050da25f7067
Arthur Dooner <shunderpooch@gmail.com>
2018-12-01 00:45:47 -0500

    Learned that Sets are way better for this sort of problem

--- day-1/chronal-calibration-2.py
+++ day-1/chronal-calibration-2.py
@@ -7,11 +7,11 @@ frequency_pattern = []
 with open("input.txt", "r") as frequency_changes:
     frequency_pattern = frequency_changes.readlines()
     
-frequencies_reached = []
+frequencies_reached = set()
 current_frequency = 0
 frequency_index = 0
 while current_frequency not in frequencies_reached:
-    frequencies_reached.append(current_frequency)
+    frequencies_reached.add(current_frequency)
     frequency_change = frequency_pattern[frequency_index % len(frequency_pattern)]
     if frequency_change[0] is '+':
         current_frequency += int(frequency_change[1:])
@@ -21,5 +21,5 @@ while current_frequency not in frequencies_reached:
     #print(frequencies_reached)
 
 
-print(frequencies_reached)
+#print(frequencies_reached)
 print(f'The resulting frequency, the answer to your version of Chronal Calibration Part 2, is {current_frequency}')
