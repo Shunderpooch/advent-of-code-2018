35077d10eb3a0df18653a9dae733212fef625a52
Arthur Dooner <shunderpooch@gmail.com>
2018-12-05 09:29:08 -0500

    Finished Part 2 of Day 2

--- day-2/inventory-management-systems-2.py
+++ day-2/inventory-management-systems-2.py
@@ -4,7 +4,7 @@
 
 count_two = []
 count_three = []
-
+count_all = []
 with open("input.txt", "r") as box_ids:
     for box_id in box_ids:
         visited_elements = set()
@@ -23,4 +23,19 @@ with open("input.txt", "r") as box_ids:
             count_two.append(box_id)
         if matches_3_elements:
             count_three.append(box_id)
-    print(f'The output checksum is {len(count_two) * len(count_three)}')
+        if matches_2_elements or matches_3_elements:
+            count_all.append(box_id)
+    result = None
+    element = count_all.pop()
+    while result is None:
+        for another_element in count_all:
+            if sum(char1 != char2 for char1, char2 in zip(element, another_element)) is 1:
+                result = [element, another_element]
+        element = count_all.pop()
+    counter = 0
+    index = None
+    for char1, char2 in zip(result[0], result[1]):
+        if char1 != char2:
+            index = counter
+        counter += 1
+    print(f'The output of part 2, the string parts in common that are only one letter away, is {result[0][:index]}{result[0][(index + 1):]}')
